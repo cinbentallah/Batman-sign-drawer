15b8a523e05d7265f8a2935b8939c0f6fc5bc44a
Yacine Bentallah <140517850+cinbentallah@users.noreply.github.com>
2023-07-25 15:19:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import turtle
+
+start_point = ()
+
+turtle.hideturtle()
+
+turtle.bgcolor('black')
+
+turtle.title("BATMAN")
+
+
+turtle.speed(2.5)
+
+
+turtle.color('yellow', "black")
+turtle.begin_fill()
+turtle.left(90)
+turtle.circle(50, 85)
+turtle.circle(15, 110)
+turtle.right(180)
+
+turtle.circle(30, 150)
+turtle.right(5)
+turtle.forward(10)
+
+turtle.right(90)
+turtle.circle(-70, 140)
+turtle.forward(40)
+turtle.right(110)
+
+turtle.circle(100, 30)
+turtle.circle(30, 100)
+turtle.left(50)
+turtle.forward(50)
+turtle.right(145)
+
+turtle.forward(30)
+turtle.left(55)
+turtle.forward(10)
+
+turtle.forward(10)
+turtle.left(55)
+turtle.forward(30)
+
+turtle.right(145)
+turtle.forward(50)
+turtle.left(50)
+turtle.circle(30, 100)
+turtle.circle(100, 30)
+
+turtle.right(90)
+turtle.right(20)
+turtle.forward(40)
+turtle.circle(-70, 140)
+
+turtle.right(90)
+turtle.forward(10)
+turtle.right(5)
+turtle.circle(30, 150)
+
+turtle.left(180)
+turtle.circle(15, 110)
+turtle.circle(50, 85)
+
+turtle.end_fill()
